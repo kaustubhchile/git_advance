c1779888d990af01a7b1212558f15fda9f93c654
kaustubhchile <kaustubchile@gmail.com>
2024-05-12 16:47:04 +0530

    Second Commit

--- calci.sh
+++ calci.sh
@@ -4,9 +4,14 @@
 aa=44+55
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 # Multiplication Function
 ee=55*99
 =======
 # Division Function
 pp=83/45
 >>>>>>> 064c063 (Fourth Commit)
+=======
+# Subtraction Function
+bb=98-44
+>>>>>>> 8bb5465 (Second Commit)
